0a6a677754a5d1b8efb72aefae0db72fcfe4886d
Rebecca Ervine <rebeccaervine@Stuarts-MacBook-Pro.local>
2019-03-03 08:55:25 +0000

    adding lightning dragon

---
+++ src/main/java/info/ata4/minecraft/dragon/server/entity/breeds/DragonBreedLightning.java
@@ -0,0 +1,47 @@
+/*
+ ** 2013 October 24
+ **
+ ** The author disclaims copyright to this source code.  In place of
+ ** a legal notice, here is a blessing:
+ **    May you do good and not evil.
+ **    May you find forgiveness for yourself and forgive others.
+ **    May you share freely, never taking more than you give.
+ */
+package info.ata4.minecraft.dragon.server.entity.breeds;
+
+import info.ata4.minecraft.dragon.server.entity.EntityTameableDragon;
+import net.minecraft.init.Biomes;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.DamageSource;
+
+/**
+ *
+ * @author Nico Bergemann <barracuda415 at yahoo.de>
+ */
+public class DragonBreedLightning extends DragonBreed {
+
+    DragonBreedLightning() {
+        super("lightning", 0xab39be);
+        
+        addImmunity(DamageSource.magic);
+        
+        addHabitatBlock(Blocks.END_STONE);
+        addHabitatBlock(Blocks.OBSIDIAN);
+        addHabitatBlock(Blocks.END_BRICKS);
+        
+        addHabitatBiome(Biomes.SKY);
+    }
+
+    @Override
+    public void onEnable(EntityTameableDragon dragon) {
+    }
+
+    @Override
+    public void onDisable(EntityTameableDragon dragon) {
+    }
+
+    @Override
+    public void onDeath(EntityTameableDragon dragon) {
+    }
+
+}
